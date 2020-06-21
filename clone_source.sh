74de70b51a9095c17ccf50d0d8553d31cc8d4531
even790403 <even790403@gmail.com>
2020-06-21 18:15:26 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone "https://github.com/project-openwrt/openwrt.git" --branch "openwrt-18.06" --single-branch "openwrt"
+git clone "https://github.com/even790403/openwrt.git" --branch "openwrt-19.07" --single-branch "openwrt"
 pushd "openwrt"; ./scripts/feeds update -a; ./scripts/feeds install -a; popd
